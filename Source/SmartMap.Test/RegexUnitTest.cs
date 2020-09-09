506952a35c71efb9158d0065e55ff530b07fbe93
Miguel <mmo_80@yahoo.se>
2020-09-09 19:34:56 +0200

    Removed unnecessary unittests

--- Source/SmartMap.Test/RegexUnitTest.cs
+++ Source/SmartMap.Test/RegexUnitTest.cs
@@ -1,5 +1,4 @@
 using System.Text.RegularExpressions;
-using SmartMap.ImportExportBusinesses;
 using SmartMap.Web.Util;
 using Xunit;
 
@@ -29,19 +28,6 @@ namespace SmartMap.Test
             var id = StringHelper.ReturnId(value);
 
             Assert.Equal(expected, id);
-        }
-
-        [Theory]
-        [InlineData("website", "a:3:{s:5:\"title\";s:0:\"\";s:3:\"url\";s:26:\"https://apparkingspot.com/\";s:6:\"target\";s:0:\"\";}", "https://apparkingspot.com/")]
-        [InlineData("website", "a:3:{s:5:\"title\";s:0:\"\";s:3:\"url\";s:280:\"https://goteborg.se/wps/portal/enhetskatalogen/!ut/p/z1/hYtLDoIwFEXXwtTJe7U_psVBlZqoASLthKA2pokiUVIjqxcWYLyjc3_goAbXtTFc2yE8uvY2eetEsyf5Ic2Iwp0mKW5KFIqtDdEFheO_gZtq_CGFYKe_bNhSI8kZManIJKqtWVFhZJVVHApw4MIFLKecz_zyzxjOfvj0HuxiTmLw7xn7ez2W_jQqlSRfuDIceA!!/dz/d5/L2dBISEvZ0FBIS9nQSEh/\";s:6:\"target\";s:0:\"\";}", "https://goteborg.se/wps/portal/enhetskatalogen/!ut/p/z1/hYtLDoIwFEXXwtTJe7U_psVBlZqoASLthKA2pokiUVIjqxcWYLyjc3_goAbXtTFc2yE8uvY2eetEsyf5Ic2Iwp0mKW5KFIqtDdEFheO_gZtq_CGFYKe_bNhSI8kZManIJKqtWVFhZJVVHApw4MIFLKecz_zyzxjOfvj0HuxiTmLw7xn7ez2W_jQqlSRfuDIceA!!/dz/d5/L2dBISEvZ0FBIS9nQSEh/")]
-        [InlineData("website", "a:3:{s:5:\"title\";s:0:\"\";s:3:\"url\";s:20:\"http://bagarbil.com/\";s:6:\"target\";s:0:\"\";}", "http://bagarbil.com/")]
-        [InlineData("facebook", "a:3:{s:5:\"title\";s:0:\"\";s:3:\"url\";s:56:\"https://www.facebook.com/groups/659539954080345/?fref=ts\";s:6:\"target\";s:0:\"\";}", "https://www.facebook.com/groups/659539954080345/?fref=ts")]
-        [InlineData("facebook", "a:3:{s:5:\"title\";s:0:\"\";s:3:\"url\";s:48:\"https://www.facebook.com/groups/153296841451958/\";s:6:\"target\";s:0:\"\";}", "https://www.facebook.com/groups/153296841451958/")]
-        public void Test_method_for_extracting_url_from_weird_wordpress_format(string type, string url, string expected)
-        {
-            var urlValue = BusinessFileGenerator.GetUrlFromWpString(url, type);
-
-            Assert.Equal(expected, urlValue);
         }
 
         [Theory]
